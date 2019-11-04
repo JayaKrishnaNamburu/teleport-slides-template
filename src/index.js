ea92e584b50b45eb0a4ee415f4f92ed6ff0c5d35
JayaKrishna <namburu1995@gmail.com>
2019-11-04 23:53:59 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, Fragment } from 'react'
+import ReactDOM from 'react-dom'
+import slides from './slides'
+import styles from './styles.module.css'
+
+const App = () => {
+	const [slide, setSlide] = useState(1)
+	const progress = (slide/slides.length)*100
+
+	const handleKeyDown = (e) => {
+		e.preventDefault();
+		if (e.key === 'ArrowRight' && slide < slides.length ) {
+			setSlide(slide + 1)
+		}
+		
+		if (e.key === 'ArrowLeft' && (slide > 1 && slide <= slides.length)) {
+			setSlide(slide - 1)
+		}
+	}
+
+	useEffect(() => {
+		document.addEventListener('keydown', handleKeyDown)
+		return () => {
+			document.removeEventListener('keydown', handleKeyDown)
+		}
+	}, [slide])
+
+	return (
+		<Fragment>
+		{slides[slide - 1]}
+		<div className={styles.slider}>
+			<span className={styles.progress} style={{ width: `${progress}%`}}></span>
+		</div>
+		</Fragment>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ src/slides/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import One from './one'
+
+const slides = [
+	<One />
+]
+
+export default slides
